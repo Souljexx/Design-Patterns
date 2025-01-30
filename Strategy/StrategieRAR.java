2118e369d038101a201e12ba4bb0c63982116eb2
Souljexx <84452403+Souljexx@users.noreply.github.com>
2025-01-30 09:58:34 +0100

    Update StrategieRAR.java

--- Strategy/StrategieRAR.java
+++ Strategy/StrategieRAR.java
@@ -1,9 +1,9 @@
 import java.io.File;
 
 public class StrategieRAR implements IStrategie {
-  
+    
     @Override
     public void execute(File file) {
-        System.out.println("Compressé avec RAR : "+file.getName());
+        System.out.println("Compressé avec RAR : "+file.getName() + ".rar");
     }
 }
